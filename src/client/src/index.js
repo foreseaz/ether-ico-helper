bff4162486c02f1270a59c7a0217fe337432962c
cxzhang <chenxizhang@theinitium.com>
2017-06-08 23:45:01 +0800

    clean client src

--- src/client/src/index.js
+++ src/client/src/index.js
@@ -5,7 +5,6 @@ import createBrowserHistory from 'history/createBrowserHistory';
 import 'semantic-ui-css/semantic.min.css';
 
 import App from './App';
-import './index.css';
 
 const history = createBrowserHistory();
 
