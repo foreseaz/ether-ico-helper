7bc158915130d8847ca2c536ecb1c3006dd8e7f9
cxzhang <chenxizhang@theinitium.com>
2017-06-09 22:05:01 +0800

    add sections

--- src/client/src/components/Nav/index.js
+++ src/client/src/components/Nav/index.js
@@ -6,11 +6,11 @@ const Nav = () => (
     <a className="logo item">
       ICO Insider
     </a>
-    <a className="active item">
-      <i className="flaticon-home"></i> Search
+    <a className="active item" href='#results'>
+      <i className="flaticon-home"></i> ICO List
     </a>
     <a className="item">
-      <i className="flaticon-mail"></i> ICO List
+      <i className="flaticon-mail"></i> ICO Analysis
     </a>
   </div>
 )
